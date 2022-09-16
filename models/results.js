fe57f694f9c5337bf1c6fdff5a9ed51cd8227f99
vmmaturana <vmmaturana@gmail.com>
2022-09-16 14:28:20 +0700

    Updated the Mongoose Model for the Data

--- models/results.js
+++ models/results.js
@@ -1,3 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-// Create the results moodel
+mongoose.model("results", {
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  links: [
+    {
+      title: String,
+      url: String,
+    },
+  ],
+});
